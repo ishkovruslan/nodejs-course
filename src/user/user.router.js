a8e279a3c4b243f598e9cfeaaea56e5cb4b69266
ishkovruslan <rus.ishkov.04@gmail.com>
2025-04-11 21:12:48 +0300

    Lab3

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -1,30 +1,23 @@
 const express = require('express');
-
 const db = require('../db');
-
 const { users } = require('../db/schema');
 
 const router = express.Router();
 
-
-// handle get request for path /users
-
 router.post('/users', async (request, response) => {
-
-   const { body } = request;
-
-   await db.insert(users).values(body);
-
-   return response.sendStatus(201);
-
+    const { body } = request;
+    await db.insert(users).values(body);
+    return response.sendStatus(201);
 });
 
 router.get('/users', async (request, response) => {
-
-   const users = await db.query.users.findMany();
-
-   return response.json(users);
-
+    const users = await db.query.users.findMany();
+    return response.json(users);
 });
+
+/* router.get('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({ route: 'get-users' });
+ }); */
 
 module.exports = router;
